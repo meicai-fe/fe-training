3b83c8d9d07cee8ad21f1ce6c966109b9115875d
yanweiqing <yanweiqing@meicai.cn>
2018-11-29 14:21:13 +0800

    调整demo关系

--- react-hooks-guide/src/App.js
+++ react-hooks-guide/src/App.js
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-import ZeroPure from './examples/zero/Pure.js';
 import ZeroOld from './examples/zero/Old.js';
+import ZeroPure from './examples/zero/Pure.js';
+import ZeroCompare from './examples/zero/Compare.js';
 import FirstPure from './examples/first/Pure.js';
 import FirstMixins from './examples/first/Mixins.js';
 import FirstExtend from './examples/first/Extend.js';
@@ -15,67 +16,67 @@ import FirstHooks from './examples/first/Hooks.js';
 import SecondOld from './examples/second/Old.js';
 import SecondPure from './examples/second/Pure.js';
 import SecondPure2 from './examples/second/Pure2.js';
-import SecondPure3 from './examples/second/Pure3.js';
 import SecondMix from './examples/second/Mix.js';
 import SecondMixHard from './examples/second/MixHard.js';
-import SecondMixHard2 from './examples/second/MixHard2.js';
 import ThirdReducer from './examples/third/Reducer.js';
 
 const Home = () => <div>Home</div>
 
 const MainMenu = () => (
   <div>
-    <Link to="/">
-      <button>home</button>
-    </Link>
-    <Link to="/0-pure">
-      <button>0-pure</button>
-    </Link>
-    <Link to="/0-old">
-      <button>0-old</button>
-    </Link>
-    <Link to="/1-pure">
-      <button>1-pure</button>
-    </Link>
-    <Link to="/1-extend">
-      <button>1-extend</button>
-    </Link>
-    <Link to="/1-mixins">
-      <button>1-mixins</button>
-    </Link>
-    <Link to="/1-high-order">
-      <button>1-high-order</button>
-    </Link>
-    <Link to="/1-render-props">
-      <button>1-render-props</button>
-    </Link>
-    <Link to="/1-hooks">
-      <button>1-hooks</button>
-    </Link>
-    <Link to="/2-old">
-      <button>2-old</button>
-    </Link>
-    <Link to="/2-pure">
-      <button>2-pure</button>
-    </Link>
-    <Link to="/2-pure-2">
-      <button>2-pure-2</button>
-    </Link>
-    <Link to="/2-pure-3">
-      <button>2-pure-3</button>
-    </Link>
-    <Link to="/2-mix">
-      <button>2-mix</button>
-    </Link>
-    <Link to="/2-mix-hard">
-      <button>2-mix-hard</button>
-    </Link>
-    <Link to="/2-mix-hard-2">
-      <button>2-mix-hard-2</button>
-    </Link>
-    <Link to="/3-reducer">
-      <button>3-reducer</button>
-    </Link>
+    <div>
+      <Link to="/0-pure">
+        <button>0-pure</button>
+      </Link>
+      <Link to="/0-old">
+        <button>0-old</button>
+      </Link>
+      <Link to="/0-compare">
+        <button>0-compare</button>
+      </Link>
+    </div>
+    <div>
+      <Link to="/1-pure">
+        <button>1-pure</button>
+      </Link>
+      <Link to="/1-extend">
+        <button>1-extend</button>
+      </Link>
+      <Link to="/1-mixins">
+        <button>1-mixins</button>
+      </Link>
+      <Link to="/1-high-order">
+        <button>1-high-order</button>
+      </Link>
+      <Link to="/1-render-props">
+        <button>1-render-props</button>
+      </Link>
+      <Link to="/1-hooks">
+        <button>1-hooks</button>
+      </Link>
+    </div>
+    <div>
+      <Link to="/2-old">
+        <button>2-old</button>
+      </Link>
+      <Link to="/2-pure">
+        <button>2-pure</button>
+      </Link>
+      <Link to="/2-pure-2">
+        <button>2-pure-2</button>
+      </Link>
+      <Link to="/2-mix">
+        <button>2-mix</button>
+      </Link>
+      <Link to="/2-mix-hard">
+        <button>2-mix-hard</button>
+      </Link>
+    </div>
+    <div>
+      <Link to="/3-reducer">
+        <button>3-reducer</button>
+      </Link>
+    </div>
   </div>
 )
 
@@ -100,6 +101,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/0-pure" component={ZeroPure} />
             <Route exact path="/0-old" component={ZeroOld} />
+            <Route exact path="/0-compare" component={ZeroCompare} />
             <Route exact path="/1-pure" component={FirstPure} />
             <Route exact path="/1-extend" component={FirstExtend} />
             <Route exact path="/1-mixins" component={FirstMixins} />
@@ -109,10 +111,8 @@ class App extends Component {
             <Route exact path="/2-old" component={SecondOld} />
             <Route exact path="/2-pure" component={SecondPure} />
             <Route exact path="/2-pure-2" component={SecondPure2} />
-            <Route exact path="/2-pure-3" component={SecondPure3} />
             <Route exact path="/2-mix" component={SecondMix} />
             <Route exact path="/2-mix-hard" component={SecondMixHard} />
-            <Route exact path="/2-mix-hard-2" component={SecondMixHard2} />
             <Route exact path="/3-reducer" component={ThirdReducer} />
           </div>
         </div>
