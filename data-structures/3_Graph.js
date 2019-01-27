9a70dabec4607ecd120470b33ec06b19daf25cfd
yanweiqing <yanweiqing@meicai.cn>
2019-01-27 16:39:53 +0800

    feat: 添加排序搜索、斐波那契额数列算法

--- data-structures/3_Graph.js
+++ data-structures/3_Graph.js
@@ -20,20 +20,13 @@ function display() {
 function dsf(v) {
   this.marked[v] = true;
 
+  console.log('visited: ' + v);
   if (this.adj[v]) {
-    let sons = this.adj[v].filter(item => {
-      return !this.marked[item]
-    });
-
-    if (sons.length > 0) {
-      sons.forEach(item => {
-        console.log('visited: ' + v);
+    this.adj[v].forEach(item => {
+      if (!this.marked[item]) {
         this.dsf(item);
-      });
-    } else {
-      console.log('visited: ' + v);
-      console.log('------------------------------');
-    }
+      }
+    });
   }
 }
 
@@ -45,13 +38,47 @@ function bsf(s) {
   queue.push(s);
 
   while (queue.length > 0) {
-    let v = queue.shift();
+    let v = queue.shift();  // 拿到队首的元素
+    console.log('visited: ' + v);
+    if (this.adj[v]) {
+      this.adj[v].forEach(item => {
+        if (!this.marked[item]) {
+          this.edgeTo[item] = v;
+          this.marked[item] = true;
+          queue.push(item);
+        }
+      });
+    }
   }
+}
+
+// 搜索路线
+function pathTo(v) {
+  let source = 0;
+  if (!this.marked[v]) {
+    return false;
+  }
+
+  let path = [];
+
+  for (let i = v; i !== source; i = this.edgeTo[i]) {
+    path.push(i);
+  }
+
+  path.push(source);
+
+  return path;
+}
+
+// 拓扑排序
+function topSort() {
+  // 还未实现，目前的数据结构是无序的，不支持
 }
 
 function initMark() {
   for (let i = 0; i < this.vertices; i ++) {
     this.marked[i] = false;
+    this.edgeTo = [];
   }
 }
 
@@ -60,6 +87,8 @@ function Graph(v) {
   this.edges = 0;
   this.adj = [];
   this.marked = [];
+  this.edgeTo = [];
+  this.vertexList = [];
 
   for (let i = 0; i < this.vertices; i ++) {
     this.adj[i] = [];
@@ -70,17 +99,56 @@ function Graph(v) {
   this.initMark = initMark;
   this.dsf = dsf;
   this.bsf = bsf;
+  this.pathTo = pathTo;
+  this.topSort = topSort;
 }
 
 g = new Graph(5);
-g.addEdge(0,1);
-g.addEdge(0,2);
-g.addEdge(1,3);
-g.addEdge(2,4);
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(2, 4);
 g.display();
 
 g.initMark();
+console.log('深度优先搜索');
 g.dsf(0);
 
 g.initMark();
+console.log('广度优先搜索');
 g.bsf(0);
+
+g = new Graph(10);
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(2, 4);
+g.addEdge(0, 5);
+g.addEdge(5, 6);
+g.addEdge(6, 4);
+g.addEdge(6, 3);
+g.addEdge(5, 7);
+g.addEdge(7, 8);
+g.addEdge(8, 9);
+g.display();
+
+g.initMark();
+console.log('深度优先搜索');
+g.dsf(0);
+
+g.initMark();
+console.log('广度优先搜索');
+g.bsf(0);
+
+console.log('------------------------------');
+console.log(g.pathTo(6));
+
+g = new Graph(6);
+g.addEdge(1, 2);
+g.addEdge(2, 5);
+g.addEdge(1, 3);
+g.addEdge(1, 4);
+g.addEdge(0, 1);
+g.vertexList = ['CS1', 'CS2', 'Data Structures', 'Assembly Language', 'Operating Systems', 'Algorithms'];
+g.display();
+g.topSort();
